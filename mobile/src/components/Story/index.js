f3b804e3ab83d038ac4432059edfe0bdb46268b5
Jéferson Pinheiro <60162736+ojeffpinheiro01@users.noreply.github.com>
2020-12-25 16:24:32 -0300

    Update index.js

--- mobile/src/components/Story/index.js
+++ mobile/src/components/Story/index.js
@@ -1,14 +1,32 @@
 import React from 'react'
+import { TextInput } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome5'
 
 import { 
     Container,
-    Image
+    InfoUser,
+    AvatarUser,
+    Username,
+    Footer,
+    Image,
+    Mensage,
+    MensageText,
 } from './styles'
 
 export default function Story({ story, properties }) {
     return(
         <Container {...properties} >
-            <Image source={{ uri: story.img }} />
+            <Image source={{ uri: story.img }} resizeMode = 'stretch' />
+            <InfoUser>
+                <AvatarUser source={{ uri: story.avatar }} resizeMode = 'contain' />
+                <Username>{story.username}</Username>
+            </InfoUser>
+            <Footer>
+                <Icon name="camera" color="white" size={28} />
+                <MensageText placeholder='Enviar mensagem' placeholderTextColor='white' editable={true} />
+                <MensageText  />
+                <Icon name="paper-plane" color="white" size={28} />
+            </Footer>
         </Container>
     )
 }
