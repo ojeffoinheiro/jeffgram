0c826db557750d0c05f2c14021508e1ea60a3e39
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-01-16 16:09:29 -0300

    add header e bio na tela de perfil

--- mobile/src/pages/Tab/Profile/index.js
+++ mobile/src/pages/Tab/Profile/index.js
@@ -1,15 +1,80 @@
-import React, { useState } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome'
+import {useNavigation} from '@react-navigation/native';
+import React, {useState} from 'react';
+import {Text, TouchableOpacity, View} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import {
+  Header,
+  Username,
+  AddPhoto,
+  Menu,
+  Container,
+  SubHeader,
+  StoryItem,
+  ContainerPhotoUser,
+  UserPhoto,
+  ContainerInfo,
+  InfoItem,
+  InfoText,
+  NumberText,
+  UserBio,
+  UsernameBio,
+  AccountBio,
+  EditInfoButton,
+  EditInfoButtonText
+} from './style';
 
 const Profile = () => {
-  const [showStory, setShowStory] =  useState(false);
+  const [showStory, setShowStory] = useState(false);
+  const avatar =
+    'https://scontent.fpoa28-1.fna.fbcdn.net/v/t1.0-9/101185565_2178567235621824_7001971009553170432_o.jpg?_nc_cat=101&ccb=2&_nc_sid=09cbfe&_nc_eui2=AeGAWX2xdunHhW9_tOCnGlkIPMIS3sk_jW88whLeyT-Nb2P7ye8AzZriP_bwlsxZ3pRamgeiNLdVVyu_jXF_X7oE&_nc_ohc=g9Bg4xGlqv4AX_BCYTN&_nc_ht=scontent.fpoa28-1.fna&oh=97d12b7ea1b113acf11550b8aaf59ea8&oe=6029BF95';
+  const navigation = useNavigation();
 
-  return(
+  const handleClick = () => {
+    navigation.navigate('Story');
+  };
+
+  return (
     <React.Fragment>
-      <View>
-        <Text>jajkgkjG</Text>
-      </View>
+      <Header>
+        <AddPhoto>
+          <Icon name="plus" size={20} />
+        </AddPhoto>
+        <Username>nome-usuario</Username>
+        <Menu>
+          <Icon name="bars" size={20} />
+        </Menu>
+      </Header>
+      <Container>
+        <SubHeader>
+          <StoryItem>
+            <ContainerPhotoUser onPress={handleClick}>
+              <UserPhoto source={{uri: avatar}} />
+            </ContainerPhotoUser>
+          </StoryItem>
+          <ContainerInfo>
+            <InfoItem>
+              <NumberText>10</NumberText>
+              <InfoText>Posts</InfoText>
+            </InfoItem>
+            <InfoItem>
+              <NumberText>100</NumberText>
+              <InfoText>Followers</InfoText>
+            </InfoItem>
+            <InfoItem>
+              <NumberText>150</NumberText>
+              <InfoText>Following</InfoText>
+            </InfoItem>
+          </ContainerInfo>
+        </SubHeader>
+        <UserBio>
+          <UsernameBio>nome-do-usuario</UsernameBio>
+          <AccountBio>texto sobre o usuario teste texto grande teste texto grandeteste texto grandeteste texto grandeteste texto grande</AccountBio>
+          <EditInfoButton>
+            <EditInfoButtonText>Editar perfil</EditInfoButtonText>
+          </EditInfoButton>
+        </UserBio>
+      </Container>
     </React.Fragment>
   );
 };
---
+++ mobile/src/pages/Tab/Profile/style.js
@@ -0,0 +1,88 @@
+import styled from 'styled-components/native';
+
+export const Header = styled.View`
+    width: 100%
+    margin-top: 15px;
+    padding: 10px;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`;
+export const AddPhoto = styled.TouchableOpacity``;
+export const Username = styled.Text``;
+export const Menu = styled.TouchableOpacity``;
+
+export const Container = styled.ScrollView``;
+export const StoryItem = styled.View`
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+export const ContainerPhotoUser = styled.TouchableOpacity`
+  background-color: #f89b3b;
+  width: 80px;
+  height: 80px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 40px;
+`;
+export const UserPhoto = styled.Image.attrs({
+  resizeMode: 'cover',
+})`
+  width: 70px;
+  height: 70px;
+  border-radius: 70px;
+  border-width: 1px;
+  border-color: #fff;
+`;
+export const SubHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 20px;
+  margin-left: 10px;
+`;
+export const ContainerInfo = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-self: center;
+  margin-end: 20px;
+`;
+export const InfoItem = styled.TouchableOpacity`
+  flex-direction: column;
+  flex: 1;
+  justify-content: space-between;
+`;
+export const InfoText = styled.Text`
+  align-self: center;
+`;
+export const NumberText = styled.Text`
+  font-weight: bold;
+  align-self: center;
+  font-size: 15px;
+`;
+
+export const UserBio = styled.View`
+  flex: 1;
+  flex-direction: column;
+  margin-start: 10px;
+  margin-top: 20px;
+`;
+export const UsernameBio = styled.Text`
+  font-weight: bold;
+  margin-bottom: 5px;
+`;
+export const AccountBio = styled.Text`
+  margin-bottom: 5px;
+`;
+export const EditInfoButton = styled.TouchableOpacity`
+  margin-top: 10px;
+  flex: 1;
+  height: 30px;
+  border-radius: 5px;
+  margin-start: 10px;
+  margin-end: 10px;
+  justify-content: center;
+  border-color: #dbdbdb;
+  border-width: 1px;
+  align-items: center`;
+export const EditInfoButtonText = styled.Text``;
