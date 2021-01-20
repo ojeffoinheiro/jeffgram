ff61acf6d89ec0d807eec71d2c84c14300e1a9ec
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-01-19 22:13:13 -0300

    alterando tab

--- mobile/src/stacks/MainTab.js
+++ mobile/src/stacks/MainTab.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,11 +12,47 @@ import Profile from '~/pages/Tab/Profile';
 import Tabbar from '~/components/Tabbar';
 
 export default () => (
-  <Tab.Navigator tabBar={props => <Tabbar {...props} /> } >
-    <Tab.Screen name="Home" component={Home} />
-    <Tab.Screen name="Search" component={Search} />
-    <Tab.Screen name="AddPhoto" component={AddPhoto} />
-    <Tab.Screen name="Notification" component={Notification} />
-    <Tab.Screen name="Profile" component={Profile} />
+  <Tab.Navigator 
+    initialRouteName="Home"
+    activeColor="#000"
+    tabBarOptions={{
+      activeTintColor: '#F89B3B',
+      showLabel: false,
+      showIcon: true
+    }}
+     >
+    <Tab.Screen name="Home" component={Home} 
+      options={{
+        showLabel: false,
+        tabBarIcon: ({ color }) => (
+          <Icon name="home" color={color} size={26} />
+        ),
+      }} />
+    <Tab.Screen name="Search" component={Search} 
+    options={{
+      showLabel: false,
+      tabBarIcon: ({ color }) => (
+        <Icon name="search" color={color} size={26} />
+      ),
+    }}/>
+    <Tab.Screen name="AddPhoto" component={AddPhoto} options={{
+        showLabel: false,
+        tabBarOptions: { showLabel: false},
+        tabBarIcon: ({ color }) => (
+          <Icon name="plus-square-o" color={color} size={26} />
+        ),
+      }}/>
+    <Tab.Screen name="Notification" component={Notification} options={{
+        showLabel: false,
+        tabBarIcon: ({ color }) => (
+          <Icon name="heart" color={color} size={26} />
+        ),
+      }}/>
+    <Tab.Screen name="Profile" component={Notification} options={{
+        showLabel: false,
+        tabBarIcon: ({ color }) => (
+          <Icon name="user" color={color} size={26} />
+        ),
+      }}/>
   </Tab.Navigator>
 )
